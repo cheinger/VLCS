89825df78445364ecc3f5e6bed9c431fb29bde5d
xavier.simmons <xavier.simmons@nyriad.com>
2017-06-05 14:12:32 +1200

    defined input for VOCL main method

--- src/VOCL/VOCL.java
+++ src/VOCL/VOCL.java
@@ -1,5 +1,17 @@
 package VOCL;
 
-public class VOCL {
+import weka.core.Instances;
 
+public class VOCL
+{
+    /**
+     * This is the main VOCL method. This will apply vague one-class learning on the input stream.
+     * @param stream            The stream of instances
+     * @param chunk_size        The number of instances per chunk
+     * @param num_classifiers   The number of classifiers forming the ensemble
+     */
+    public void labelStream(Instances stream, final int chunk_size, final int num_classifiers)
+    {
+
+    }
 }
